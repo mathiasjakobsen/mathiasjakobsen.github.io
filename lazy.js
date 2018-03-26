400f610dcb5b8cf62c7931b86ff6c267994db3ac
Mathias Jakobsen <mathiasjakobsen@me.com>
2018-03-26 21:51:43 +0200

    Intialize sections with thumbnails, via. CSS

--- lazy.js
+++ lazy.js
@@ -1,11 +1,9 @@
 window.addEventListener('DOMContentLoaded', function() {
   document.querySelectorAll('article').forEach(function(article) {
-    article.style.backgroundImage = 'url("' + article.dataset.thumbnail + '")'
-    article.classList.add('blurred')
     var img = new Image()
     img.src = article.dataset.image
     img.onload = function() {
-      article.classList.remove('blurred')
+      article.classList.remove('lazy')
       article.style.backgroundImage = 'url("' + article.dataset.image + '")';
     }
   })
